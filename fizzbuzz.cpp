7dcafc3852d28a409cbf47070e8408f3fd565dfe
WoundedSlug <whodaman.imdaman@gmail.com>
2016-10-18 20:45:50 -0400

    Added FizzBuzz C++

---
+++ fizzbuzz.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+  for(int c = 1; c <= 100; c++){
+      std::string output;
+      if(c % 3 == 0) output += "Fizz";
+      if(c % 5 == 0) output += "Buzz";
+      if(output.length() == 0) std::cout << c << std::endl;
+      else std::cout << output << std::endl;
+  }
+}
